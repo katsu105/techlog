a1a8877efe3bf3f2196e4d1fe9314033aedb5060
suguru_katsuyama <zgok105@yahoo.co.jp>
2019-05-19 15:06:00 +0900

    hoge

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'drafts#index'
+  resources :drafts
 end
